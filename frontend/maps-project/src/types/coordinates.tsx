c00ad3e5febfddbdbc4bd2a7c9e20aef00d0bccc
Neel Sawant <sawant.ne@northeastern.edu>
2025-05-02 16:12:14 -0400

    started coordinate interfaces

---
+++ frontend/maps-project/src/types/coordinates.tsx
@@ -0,0 +1,15 @@
+/**
+ * This interface contains data for lat / long coordinates on the map.
+ */
+interface LatLng {
+  lattitude: number;
+  longitude: number;
+}
+
+/**
+ *
+ */
+interface Point {
+  x: number;
+  y: number;
+}
